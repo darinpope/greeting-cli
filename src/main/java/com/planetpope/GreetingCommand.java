eb39bbeaadc23d9151e852749ef0d1754d5cf398
Darin Pope <darin@planetpope.com>
2022-05-02 14:27:46 -0400

    initial commit

---
+++ src/main/java/com/planetpope/GreetingCommand.java
@@ -0,0 +1,22 @@
+package com.planetpope;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+@Command(
+  name = "greeting", 
+  version = "0.0.1",
+  mixinStandardHelpOptions = true
+)
+public class GreetingCommand implements Runnable {
+    @Parameters(
+      defaultValue = "picocli",
+      description = "Your name."
+    )
+    String name;
+
+    @Override
+    public void run() {
+      System.out.printf("Hello %s, go go commando!\n", name);
+    }
+}
